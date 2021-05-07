777d323028142d9ec18fef315b8eaeaca2c31d84
unknown <khoatla@kimei.vn>
2021-05-07 13:13:35 +0700

    cung hoang dao

--- condition/Age/dbTable.php
+++ condition/Age/dbTable.php
@@ -3,7 +3,7 @@
     function getDB($day,$mon){
         $connect = connectDB();
         $times = date('Y').'-'.$mon.'-'.$day;
-        $sql = "SELECT img, name, nameEN, firstDate, lastDate
+        $sql = "SELECT img, name, nameEN, firstDate, lastDate, step
                 FROM `table1` WHERE 
                 (STR_TO_DATE('$times','%Y-%m-%d') BETWEEN STR_TO_DATE(CONCAT(DATE_FORMAT(now(),'%Y'),'-',firstDate),'%Y-%m-%d') AND STR_TO_DATE(DATE_ADD(CONCAT(DATE_FORMAT(now(),'%Y'),'-',lastDate),INTERVAL `step` YEAR),'%Y-%m-%d')) OR
                 (STR_TO_DATE('$times','%Y-%m-%d') BETWEEN STR_TO_DATE(DATE_ADD(CONCAT(DATE_FORMAT(now(),'%Y'),'-',firstDate),INTERVAL -`step` YEAR),'%Y-%m-%d') AND STR_TO_DATE(CONCAT(DATE_FORMAT(now(),'%Y'),'-',lastDate),'%Y-%m-%d')) ";
@@ -11,16 +11,13 @@
         if($result->num_rows > 0) {
             $rows = $result->fetch_all(MYSQLI_ASSOC); 
             //     echo '<pre>';
-            // print_r($row[0][3]);
+            // print_r($rows[3]);
             // die();
-            //     $khoa =   '<div class="row">
-            //                     <img src="../Age/img/'. $row["ïmg"] .'" alt="'. $row["ïmg"] .'">
-            //                     <p>'. $row["name"] .' <span> ('. $row["nameEN"] .' : '. $row["time"] .') </span></p>
-            //                 </div>';
-            // }
             return $rows;
         //}
+        } else { 
+            return false;
+        }
     }
-}
     
 ?>
--- condition/Age/index.php
+++ condition/Age/index.php
@@ -1,10 +1,6 @@
 <?php 
 include 'dbTable.php';
 
-
-
-
-
 $day = "";
 $mon = "";
 
@@ -13,18 +9,21 @@ $name   = "";
 $nameEN = "";
 $firstDate = "";
 $lastDate  = "";
-
-
+$getStar[0]['img'] = "";
+$getStar[0]['name'] = "";
+$getStar[0]['nameEN'] = "";
+$getStar[0]['firstDate'] = "";
+$getStar[0]['lastDate'] = "";
 
 if(isset($_POST["day"]) && isset($_POST["mon"])){
     $day = $_POST["day"];
     $mon = $_POST["mon"];
     $getStar = getDB($day, $mon);
     // echo '<pre>';
-    // print_r($getStar);
-
+    // print_r($getStar[0]['img']);
+ 
 }
-if((is_numeric($day)) && (is_numeric($mon))){
+
     /**
      * Capricorn  23/12 - 19/1   Ma Ket
      * Aquarius   20/01 - 19/02  Bao Binh
@@ -35,50 +34,12 @@ if((is_numeric($day)) && (is_numeric($mon))){
      * Cancer     23/06 - 23/07  Cu Giai
      * Leo        24/07 - 23/08  Su Tu
      * Virgo      24/08 - 23/09  Xu Nu
-     * Libra      24/09 - 23/10  Thien Xung
+     * Libra      24/09 - 23/10  Thien binh
      * Scorpio    24/10 - 22/11 Ho Cap
      * Sagittarius  23/11 - 22/12 Nhan ma
     */
-    switch ($mon) {
-        case '1':
-            if($day <= 19) { $img = $getStar[0]['img']; $name   = $getStar[0]["name"]; $nameEN = $getStar[0]["nameEN"]; $time   = $getStar[0]["firstDate"]; $getStar[0]["lastDate"];} 
-            if($day >= 20) { $img = $getStar[1]['img']; $name   = $getStar[1]["name"]; $nameEN = $getStar[1]["nameEN"]; $time   = $getStar[1]["firstDate"]; $getStar[1]["lastDate"];}
-        break;
-        case '2':
-            if($day <= 19) { $img = "CungBaoBinh.png"; $name   = "Cung Bao Binh";$nameEN = "Aquarius";$time   = "20/01 - 19/02";}
-            if($day >= 20) {$img = "CungSongNgu.png"; $name   = "Cung Song Ngu"; $nameEN = "Pisces"; $time   = "20/02 - 21/03";}
-        break;
-        case '3':
-            if($day <= 21) {$img    = "CungSongNgu.png"; $name   = "Cung Song Ngu";$nameEN = "Pisces";$time   = "20/02 - 21/03";}
-            if($day >= 22) {$img  = "CungBachDuong.png";$name   = "Cung Bach Duong"; $nameEN = "Aries";$time   = "22/03 - 20/04";}
-        break;
-        case '4':
-            if($day <= 20) {$img  = "CungBachDuong.png";$name   = "Cung Bach Duong"; $nameEN = "Aries";$time   = "22/03 - 20/04";}
-            if($day >= 21) {$img  = "CungKimNguu.png";$name   = "Cung Kim Nguu"; $nameEN = "Taurus";$time   = "21/04 - 21/05";}
-        break;
-        case '5':
-            if($day <= 21) {$img  = "CungKimNguu.png";$name   = "Cung Kim Nguu"; $nameEN = "Taurus";$time   = "21/04 - 21/05";}
-            if($day >= 22) {$img  = "CungSongTu.png";$name   = "Cung Song Tu"; $nameEN = "Gemini";$time   = "22/05 - 22/06";}
-        break;
-        case '6':
-            if($day <= 22) {$img  = "CungSongTu.png";$name   = "Cung Song Tu"; $nameEN = "Gemini";$time   = "22/05 - 22/06";}
-            if($day >= 23) {$img  = "CungCuGiai.png";$name   = "Cung Cu Giai"; $nameEN = "Cancer";$time   = "23/06 - 23/07";}
-        break;
-        case '7':
-            if($day <= 23) {$img  = "CungSongTu.png";$name   = "Cung Song Tu"; $nameEN = "Gemini";$time   = "22/05 - 22/06";}
-            if($day >= 23) {$img  = "CungCuGiai.png";$name   = "Cung Cu Giai"; $nameEN = "Cancer";$time   = "23/06 - 23/07";}
-        break;
-        case '8':
-            if($day <= 22) {$img  = "CungSongTu.png";$name   = "Cung Song Tu"; $nameEN = "Gemini";$time   = "22/05 - 22/06";}
-            if($day >= 23) {$img  = "CungCuGiai.png";$name   = "Cung Cu Giai"; $nameEN = "Cancer";$time   = "23/06 - 23/07";}
-        break;
-            
-        
-        default:
-            # code...
-            break;
-    }
-}
+ 
+
 
 ?>
 <!doctype html>
@@ -90,23 +51,22 @@ if((is_numeric($day)) && (is_numeric($mon))){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="style.css">
-    <!-- Bootstrap CSS -->
 </head>
 
 <body>
     <div class="content">
-        <h2>Ban thuoc chom sao nao ?</h2>
+        <h2>Kiệt Thạch thuộc chòm sao nào ?</h2>
         <form method="POST" action="#" name="main-from">
             <div class="row">
-                <label>Ngay sinh:</label>
+                <label>Ngày sinh:</label>
                 <input type="text" name="day" value="<?php echo $day ?>">
             </div>
             <div class="row">
-                <label>Thang sinh:</label>
+                <label>Tháng sinh:</label>
                 <input type="text" name="mon" value="<?php echo $mon ?>">
             </div>
             <div class="row">
-                <input type="submit" value="Click vao de xem chom sao ^.^" name="submit">
+                <input type="submit" value="Click vào để xem chòm sao ^.^" name="submit">
             </div>
         </form>
         <!-- <div class="row">
@@ -114,12 +74,16 @@ if((is_numeric($day)) && (is_numeric($mon))){
                 <p>Cung bach duong <span> (Aries : 21/3 - 20/4) </span></p>
         </div> -->
         <?php 
+        if ($getStar !== false) {
             $result =   '<div class="row">
-                            <img src="../Age/img/'. $img .'" alt="'. $img .'">
-                            <p>'. $name .' <span> ('. $nameEN .' : '. $firstDate .''. $lastDate.') </span></p>
+                            <img src="../Age/img/'. $getStar[0]['img'] .'" alt="'. $getStar[0]['img'] .'">
+                            <p>'. $getStar[0]['name'] .' <span> ('.$getStar[0]['nameEN'].' : '.$getStar[0]['firstDate'] .' '. $getStar[0]['lastDate'].') </span></p>
                         </div>';
             echo $result;
-           
+        } else {
+            
+            echo 'Nhập thông ngày tháng';
+        }
         ?>
     </div>
 </body>
